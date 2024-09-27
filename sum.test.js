e7de0d561f2fe02fc2aecbe5300183d40dd72744
Neji <newtonombese1@gmail.com>
2024-09-27 08:22:05 +0300

    Setting up tests for negative and positive and test for 0

--- sum.test.js
+++ sum.test.js
@@ -5,6 +5,14 @@ describe('Sum function', () => {
     expect(sum(1, 2)).toBe(3)
   });
 
+  it('should return -1 when passed -2 and 1', () => {
+    expect(sum(-2, 1)).toBe(-1);
+  });
+
+  it('should return 0 when 0 and 0 are passed', () => {
+    expect(sum(0, 0)).toBe(0)
+  })
+
   it('should throw an error if one or all parameters are not numbers', () => {
     expect(() => sum(2, '3')).toThrow('parameters must be numbers');
   });
